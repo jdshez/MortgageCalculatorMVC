7e025e485691c55c543e2ef43ef7fd1b1d5bd57b
jdshez <jd_shez@protonmail.com>
2021-08-09 23:47:48 +0100

    Add project files.

---
+++ CSharpMortgageCalculator/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using CSharpMortgageCalculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpMortgageCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult App()
+        {
+            Loan loan = new();
+
+            loan.Payment = 0.0m;
+            loan.TotalInterest = 0.0m;
+            loan.TotalCost = 0.0m;
+            loan.Rate = 3.5m;
+            loan.Amount = 15000m;
+            loan.Term = 60;
+
+            return View(loan);
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult App(Loan loan)
+        {
+            //Calc loan 
+
+            return View(loan);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSharpMortgageCalculator/Helpers/LoanHelper.cs
@@ -0,0 +1,35 @@
+﻿using CSharpMortgageCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharpMortgageCalculator.Helpers
+{
+    public class LoanHelper
+    {
+        public Loan GetPayments(Loan loan)
+        {
+            //Calc monthly payment
+
+            //Create a loop from 1 to term length
+
+            //Calc payment schedule
+
+            //push payments into the loan
+
+
+            //return the loan
+            return loan;
+        }
+
+        private decimal CalcPayment(decimal amount, decimal rate, int term)
+        {
+            decimal payment = 0.0m;
+
+
+
+            return payment;
+        }
+    }
+}
